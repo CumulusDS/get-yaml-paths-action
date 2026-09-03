92469093d093b35d6dbec5690d87188ce9076d81
Jeff <49158344+jeffsays@users.noreply.github.com>
2026-09-03 16:33:56 -0400

    Revert "chore: bump `js-yaml` to resolve GHSA-5p4m-2wfm-xmqj"

--- .babelrc.js
+++ .babelrc.js
@@ -4,7 +4,7 @@ module.exports = {
       "@babel/preset-env",
       {
         targets: {
-          node: "24.19.0",
+          node: "12.13.0",
         },
         exclude: ["proposal-async-generator-functions"], // Do not transform async generator functions, since they are supported natively in Node 10 & 12.
       },
